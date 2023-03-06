5b6e2714fa37bb3caf533f93ea81997a4a092483
JyunghyeC <126124026+JyunghyeC@users.noreply.github.com>
2023-03-06 22:04:02 +0900

    [level 0] Title: 369게임, Time: 0.02 ms, Memory: 74.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120891. 369게임/369게임.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int order) {
+        int answer = 0;
+        
+        String str = Integer.toString(order);
+		
+		for(int i=0;i<str.length();i++) {
+			char ch = str.charAt(i);
+			
+			if(ch == '3' || ch == '6' || ch == '9')
+	              answer += 1;
+		}
+        return answer;
+    }
+}
